537b3bc628b62437e1cdafb24a8651757cde6e61
Nani7008 <narimonjon05@gmail.com>
2024-03-03 02:03:22 +0500

    added popups and connected js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -16,6 +16,21 @@
 
 const iconUser = document.getElementById('icon-user');
 const login = document.getElementById('login');
+const close = document.getElementById('close')
+
+
+
+
+
+
+
+
+
+
+
 iconUser.addEventListener('click', function() { 
 login.classList.add('active-login');    
  } );
+ close.addEventListener('click' , function () {
+    login.classList.remove('active-login');
+ })
