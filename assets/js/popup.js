f289a90140684204a5e906b9af2b0a2928ecb058
Nani7008 <narimonjon05@gmail.com>
2024-03-06 00:30:17 +0500

    description section finished

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,18 +1,4 @@
-// var toggleButton = document.getElementById("toggle");
-// var iconUser = document.getElementById("icon-user");
-// var myLogin = document.getElementById("login");
 
-
-
-// toggleButton.addEventListener('click', function () {
-
-//     login.classList.toggle('hide-login');
-//     if (!myLogin.classList.contains('hide-login')) {
-//         iconUser.innerHTML = `<i class="bx bx-user"></i>`
-//     } else {
-//         iconUser.innerHTML = `<i class="bx bx-user"></i>`
-//     }
-// });
 
 const iconUser = document.getElementById('icon-user');
 const login = document.getElementById('login');
